aa363959824fdbfa1f94c1c6f49ce8ba0e2847f4
joseluisn <42726030+joseluisn@users.noreply.github.com>
2019-06-12 10:49:08 -0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 10:16:50 2019
+
+@author: JOSE LUIS NOVOA
+"""
+
+def sum(x,y):
+    print("La suma de %d+%d es:%d "%(x,y,x+y))
+
+def res(x,y):
+    print("La resta de %d-%d es:%d "%(x,y,x-y))
+
+def mul(x,y):
+    print("La multiplicacion de %d*%d es:%d "%(x,y,x*y))
+def div(x,y):
+    print("La division de %d/%d es:%d"%(x,y,x/y))
+def resi(x,y):
+    print ("El residuo de %d / %d es:%d"%(x,y,x%y))
